71c5796ea75ff56b867699c3ce086c30639570bd
Tess Rezard <tessrezard@gmail.com>
2024-02-13 15:01:33 +0000

    pare back

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Root from './Root';
 import Home from './pages/Home';
-import Thread from './pages/Thread';
-import Subreddit from './pages/Subreddit';
-import SearchResults from './pages/SearchResults';
 
 function App() {
 
@@ -15,9 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Root />}>
             <Route path="home" element={<Home />} />
-            <Route path="r/:subreddit" element={<Subreddit />} />
-            <Route path="comments/:thread" element={<Thread />} />
-            <Route path="search/:term" element={<SearchResults />} />
           </Route>
         </Routes>
       </Router>
